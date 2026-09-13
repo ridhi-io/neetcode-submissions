cb89e1002133c407fd636181242c0c69be0701e5
ridhi-io <ridhi.24bcy10260@vitbhopal.ac.in>
2026-09-13 22:16:37 +0530

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> st;
+
+        while(true) {
+            while(root != nullptr) {
+                st.push(root);
+                root = root->left;
+            }
+
+            root = st.top();
+            st.pop();
+
+            k--;
+
+            if(k == 0)
+                return root->val;
+
+            root = root->right;
+        }
+    }
+};
