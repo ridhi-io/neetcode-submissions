9ba4cd392303160f5ae2d7211dcd1ac59a9cd659
ridhi-io <ridhi.24bcy10260@vitbhopal.ac.in>
2026-09-16 22:00:21 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+public:
+    priority_queue<int , vector<int>,greater<int>> minHeap;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int num : nums){
+            add(num);
+        }
+    }
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size() > k){
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
